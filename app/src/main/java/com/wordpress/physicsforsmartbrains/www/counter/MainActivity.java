3e6fb566add045a0b419696819ea5e0cd595b05c
ANSHUL YADAV <anshulyadav1911@gmail.com>
2017-12-10 21:00:08 +0530

    Added Splash screen and Theme Changes

--- app/src/main/java/com/wordpress/physicsforsmartbrains/www/counter/MainActivity.java
+++ app/src/main/java/com/wordpress/physicsforsmartbrains/www/counter/MainActivity.java
@@ -10,40 +10,41 @@ import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
 import static android.icu.lang.UCharacter.GraphemeClusterBreak.V;
 
 public class MainActivity extends AppCompatActivity {
-private int count=0;
+    private int count = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button=(Button) findViewById(R.id.increment);
-        Button button1=(Button) findViewById(R.id.decrement);
-        Button reset=(Button) findViewById(R.id.reset);
+        Button button = (Button) findViewById(R.id.increment);
+        Button button1 = (Button) findViewById(R.id.decrement);
+        Button reset = (Button) findViewById(R.id.reset);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                count=count+1;
+                count = count + 1;
                 String score = new StringBuffer().append(count).toString();
-                TextView textView=(TextView) findViewById(R.id.count);
-               textView.setText(score);
+                TextView textView = (TextView) findViewById(R.id.count);
+                textView.setText(score);
             }
         });
-        button1.setOnClickListener(new View.OnClickListener(){
+        button1.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view){
-               count=count-1;
+            public void onClick(View view) {
+                count = count - 1;
                 String score = new StringBuffer().append(count).toString();
-                TextView textView=(TextView) findViewById(R.id.count);
+                TextView textView = (TextView) findViewById(R.id.count);
                 textView.setText(score);
             }
         });
         reset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                count=0;
+                count = 0;
                 String score = new StringBuffer().append(count).toString();
-                TextView textView=(TextView) findViewById(R.id.count);
-                textView.setText(score); 
+                TextView textView = (TextView) findViewById(R.id.count);
+                textView.setText(score);
             }
         });
 
